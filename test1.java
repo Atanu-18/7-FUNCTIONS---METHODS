d2cb3e01eec2a6a9670499c5273b504ebfac6541
atanu mandal <atanu2697@gmail.com>
2025-05-01 10:44:50 +0530

    test2..

---
+++ test1.java
@@ -0,0 +1,24 @@
+// Make a function to add 2 no's & return the sum
+
+import java.util.Scanner;
+
+public class test1 {
+
+    // function declaration 
+    public static int sumNumber(int n1, int n2) {
+        int sum = n1 + n2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int sum = sumNumber(n1, n2);
+        System.out.println("Sum of two no is : " + sum);
+
+        sc.close();
+    }
+}
