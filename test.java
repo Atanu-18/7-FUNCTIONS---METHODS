fdef37669753512ddd2af71165bd6b3be9b76b61
atanu mandal <atanu2697@gmail.com>
2025-06-19 22:12:48 +0530

    add

--- test.java
+++ test.java
@@ -9,6 +9,7 @@ public class test {
         System.out.println(name);
         return;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
